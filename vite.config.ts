a0d440b8685df82cbaa07f297a13a347cb95078f
marcoelissa <marco.elissa@gmail.com>
2022-12-06 03:31:46 +0700

    remove library build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
 import dts from 'vite-plugin-dts'
-import * as packageJson from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,16 +17,6 @@ export default defineConfig({
     }
   },
   build: {
-    target: 'es2020',
-    // Running build script will create the library
-    lib: {
-      entry: resolve(__dirname, 'src/lib/index.ts'),
-      name: 'orbis-modules',
-      formats: ['es', 'umd'],
-      fileName: (format) => `orbis-modules.${format}.js`
-    },
-    rollupOptions: {
-      external: [...Object.keys(packageJson.peerDependencies)]
-    }
+    target: 'es2020'
   }
 })
