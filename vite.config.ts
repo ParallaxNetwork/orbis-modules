36e73e25f65d2b1bddb4ba1d1e93a63d0cde90cb
marcoelissa <marco.elissa@gmail.com>
2022-12-06 01:14:41 +0700

    comment the build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
+// import { resolve } from 'node:path'
 import dts from 'vite-plugin-dts'
-import * as packageJson from './package.json'
+// import * as packageJson from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,14 +19,14 @@ export default defineConfig({
   },
   build: {
     target: 'es2020',
-    lib: {
-      entry: resolve(__dirname, 'src/lib/index.ts'),
-      name: 'orbis-react-modules',
-      formats: ['es', 'umd'],
-      fileName: (format) => `orbis-react-modules.${format}.js`
-    },
-    rollupOptions: {
-      external: [...Object.keys(packageJson.peerDependencies)]
-    }
+    // lib: {
+    //   entry: resolve(__dirname, 'src/lib/index.ts'),
+    //   name: 'orbis-modules',
+    //   formats: ['es', 'umd'],
+    //   fileName: (format) => `orbis-modules.${format}.js`
+    // },
+    // rollupOptions: {
+    //   external: [...Object.keys(packageJson.peerDependencies)]
+    // }
   }
 })
